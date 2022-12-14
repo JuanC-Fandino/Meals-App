5bbdec969db6916c35f19efa759465c8d9152e90
Juan Camilo Fandiño Orjuela <juancamilo1998@hotmail.com>
2022-12-14 10:35:30 -0500

    chore: remove test files

--- src/components/IconButton/IconButton.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from '@testing-library/react-native';
-import IconButton from './IconButton';
-
-test('IconButton: renders without crashing', async () => {
-  const result = render(<IconButton />)
-
-  expect(result).toBeTruthy();
-});
